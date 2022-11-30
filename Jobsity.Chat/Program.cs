c1b961cdfa7924d1a6b337959afc055bc2128048
Guilherme Souza <guirsz@gmail.com>
2022-11-30 00:22:08 -0300

    Fixing cors configuration

--- Jobsity.Chat/Program.cs
+++ Jobsity.Chat/Program.cs
@@ -17,9 +17,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+        policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed((host) => true)
+                .AllowCredentials();
     });
 });
 
